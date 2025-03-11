52aaf62cf0672e34720ce1586d7d6920f3663b8f
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-11 09:25:48 +0530

    Program to store cart items and its information in a class

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,32 +1,41 @@
-class Cart{
-    int cartQuantity;
-    String cartName;
-    String[] cart;
+class CartItem{
+    String itemName;
+    int price;
+    int quantity;
 
-    Cart(String cartName){
-        this.cartQuantity = 0;
-        this.cartName = cartName;
-    }
-
-    void addItem(){
-
+    CartItem(){
+        this.price = 0;
+        this.quantity = 0;
     }
     
-    class item{
-        int itemPrice;
-        String itemName;
-        int itemQuantity;
-
-        item(){
-
-        }
-
+    void addItem(String itemName, int price, int quantity){
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity += quantity;
     }
+
+    void removeItem(int quantity){
+        this.quantity -= quantity;
+    }
+
+    int getTotalCost(){
+        return this.price*this.quantity;
+    }
+    void display(){
+        System.out.println("Name of the item is " + itemName);
+        System.out.println("Price of the item is " + price);
+        System.out.println("Total number of items are " + quantity);
+        System.out.println("Total cost is " + getTotalCost());
+    }
+
 }
 
 public class ShoppingCart {
 
     public static void main(String[] args){
-
+        CartItem item = new CartItem();
+        item.addItem("football", 800, 2);
+        item.removeItem(1);
+        item.display();
     }
 }
